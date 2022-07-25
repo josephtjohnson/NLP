44ff2a99e56deef0da918c07dadf71da99836ee2
Joseph Johnson <johnson_jt@outlook.com>
2022-07-24 21:50:32 -0400

    Update styling

--- webpack.dev.js
+++ webpack.dev.js
@@ -15,7 +15,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: test: /\.js$|jsx/,
+                test: /\.js$|jsx/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             },
