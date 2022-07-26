0e06b74cd1ed11d76064bfb13e88823346b89c49
Joseph Johnson <82980802+josephtjohnson@users.noreply.github.com>
2022-07-26 12:28:27 -0400

    Update tests to correct function names

--- src/__test__/urlChecker.test.js
+++ src/__test__/urlChecker.test.js
@@ -1,20 +1,16 @@
 /** * @jest-environment jsdom */
 const checkForURL = require('../client/js/urlChecker')
 
-describe ("Testing the url check functionality", () => {
-  test("Testing the checkForURL() function", () => {
-    expect(checkForURL).toBeDefined();
-  });
   test("Testing the url is not null", () => {
     const url = 'https://apnews.com/article/elon-musk-technology-a71c76ed753b144f4aee081df5ab707c';
-    expect(urlChecker(url)).toBeTruthy();
+    expect(checkForURL(url)).toBeTruthy();
   });
   test("Testing the url is null", () => {
     const url = '';
-    expect(urlChecker(url)).toBeNull();
+    expect(checkForURL(url)).toBeNull();
   });
   test("Testing the url is null", () => {
     const url = 'adsfafdhadsf';
-    expect(urlChecker(url)).toBeFalsy();
+    expect(checkForURL(url)).toBeFalsy();
   });
 });
